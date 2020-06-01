905faf29321aca4742b8cc20987bbe3923c09b52
fayaz <fayazfz07@gmail.com>
2020-06-01 13:29:17 +0530

    - removed load_toast - finished NotificationWidget configuration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ots/ots.dart';
 
-void main() => runApp(MaterialApp(
-  home: OTS(
-    child: Home(),
-  ),
-));
+void main() => runApp(
+      MaterialApp(
+        home: OTS(
+          child: Home(),
+        ),
+      ),
+    );
 
 class Home extends StatefulWidget {
   @override
@@ -13,30 +15,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final double indicatorWidth = 24.0;
-  final double indicatorHeight = 300.0;
-  final double slideHeight = 200.0;
-  final double slideWidth = 400.0;
-
-  final LayerLink layerLink = LayerLink();
-  OverlayEntry overlayEntry;
-  Offset center;
-
   @override
   Widget build(BuildContext context) {
-    center = Offset(MediaQuery.of(context).size.width / 2,
-        MediaQuery.of(context).size.height / 2);
     return Scaffold(
-      body: Container(
-        color: Colors.blue.withOpacity(0.2),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                showLoadToast(text: 'Hello');
-              },
-              child: Center(child: Text('press here to show loader')),
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              alignment: WrapAlignment.center,
+              direction: Axis.horizontal,
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.blue,
+                  child: Text('Show LoadToast'),
+                  onPressed: () {
+                    showNotification(
+                      message: 'Hello, this is notification',
+                      title: 'Test',
+                      backgroundColor: Colors.blue,
+                      autoDismissible: true,
+                      notificationDuration: 2500,
+                    );
+                  },
+                ),
+                RaisedButton(
+                  color: Colors.green,
+                  child: Text('LoadToast Success'),
+                  onPressed: () {
+//                    hideLoadToastWithSuccess();
+                  },
+                ),
+                RaisedButton(
+                  color: Colors.red,
+                  child: Text('LoadToast Error'),
+                  onPressed: () {
+//                    hideLoadToastWithError();
+                  },
+                ),
+                RaisedButton(
+                  color: Colors.orange,
+                  child: Text('LoadToast Warning'),
+                  onPressed: () {
+//                    hideLoadToastWithWarning();
+                  },
+                ),
+              ],
             ),
           ],
         ),
